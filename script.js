062fbd0474b0cc5490a7df78e75550095b378451
Shivani <60198743+shivanidotsingh@users.noreply.github.com>
2020-09-22 14:35:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-(function ($) {
-    'use strict';
-    $('.item').on("click", function () {
-        $(this).next().slideToggle(100);
-        $('p').not($(this).next()).slideUp('fast');
-    });
-}(jQuery));
+$(function() {
+	
+	window.setTimeout(function() {
+		$('section').css('opacity', '1');
+	}, 2000);
+
+	$('div').addClass('default');
+	
+		$('div').on('click', function() {
+	
+	  	var e = $('section > div');
+			if(e.hasClass('expand')){
+				 e.removeClass('expand');
+			 	$(this).addClass('expand');
+			} else { $(this).addClass('expand'); }
+		})
+})
